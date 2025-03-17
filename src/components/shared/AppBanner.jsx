e2ee746db7027811ef96aa658c8394ece1bce559
Heiddis Birta <156322797+HeiddisBirta@users.noreply.github.com>
2025-03-17 13:14:07 +0000

    something

--- src/components/shared/AppBanner.jsx
+++ src/components/shared/AppBanner.jsx
@@ -1,7 +1,7 @@
 import useThemeSwitcher from '../../hooks/useThemeSwitcher';
 import { FiArrowDownCircle } from 'react-icons/fi';
-import developerLight from '../../images/developer.svg';
-import developerDark from '../../images/developer-dark.svg';
+import developerLight from '../../images/photo.jpeg';
+import developerDark from '../../images/photo.jpeg';
 import { motion } from 'framer-motion';
 
 const AppBanner = () => {
